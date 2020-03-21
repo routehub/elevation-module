6ce305295392711cf8636e2993c6fa872eda4cef
ctyo <ctyo@live.jp>
2020-03-21 15:45:11 +0900

    tmp add dist dir

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * create Elevation Graph Object. using Chart.js
+ * @param element target Dom element.
+ * @param cooard [[lat, lng, altitude] , [lat, lng, alt]....
+ * @param option [options={}] Optional parameters
+ */
+declare function chartjs_util_elevation(element: any, cooard: any, option: any): any;
+export default chartjs_util_elevation;
---
+++ dist/index.js
@@ -0,0 +1,64 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var chart_js_1 = __importDefault(require("chart.js"));
+var length_1 = __importDefault(require("@turf/length"));
+var turf_along_1 = require("turf-along");
+/**
+ * create Elevation Graph Object. using Chart.js
+ * @param element target Dom element.
+ * @param cooard [[lat, lng, altitude] , [lat, lng, alt]....
+ * @param option [options={}] Optional parameters
+ */
+function chartjs_util_elevation(element, cooard, option) {
+    // set option 
+    var sampling = option.sampling || 0.1; // part of pos(@0.1km)
+    var backgroundColor = option.backgroundColor || "rgb(255,51,242,0.7)";
+    // normalize data
+    var total_distance = length_1.default(cooard);
+    var normalizedCooard = []; // distance normalize cooards
+    var labels;
+    for (var now = 0; now < total_distance; now += sampling) {
+        var label = (labels.length == 0 || labels.length % 5 === 0) ? Math.round(now * 1000) : null;
+        labels.push = label;
+        normalizedCooard.push(turf_along_1.turfAlong(cooard, now));
+    }
+    var normalizedLevel = normalizedCooard.map(function (p) { return p.geometry.coordinates[2]; });
+    // create graph
+    return new chart_js_1.default(element, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                    data: normalizedLevel,
+                    backgroundColor: backgroundColor,
+                    pointRadius: 0,
+                }]
+        },
+        options: {
+            title: {
+                display: false,
+            },
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                        display: true,
+                        stacked: false,
+                        gridLines: {
+                            display: false
+                        }
+                    }],
+                yAxes: [{
+                        gridLines: {
+                            drawBorder: false
+                        }
+                    }]
+            }
+        }
+    });
+}
+exports.default = chartjs_util_elevation;
