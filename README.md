c7e8a1866895c104923ef97f35831c38dbeb4a18
ctyo <ctyo@live.jp>
2020-03-21 15:05:18 +0900

    add[

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # chartjs-util-elevation
+
+Map and Elevation Graph Library. using Chart.js and turf.js.
+
+
+usage for browser
+```
+
+```
+
+usage for commonjs
+```
+
+```
+
+
+
---
+++ index.ts
@@ -0,0 +1,58 @@
+import Chart from 'chart.js'
+import turfLength from '@turf/length'
+import { turfAlong } from 'turf-along'
+
+export default function (element, cooard, option) {
+    // set option 
+    const sampling = option.sampling || 0.1 // part of pos(@0.1km)
+    const backgroundColor = option.backgroundColor || "rgb(255,51,242,0.7)";
+
+    // normalize data
+    const total_distance = turfLength(cooard)
+    let normalizedCooard = [] // distance normalize cooards
+    let labels
+    for (let now = 0; now < total_distance; now += sampling) {
+        const label = (labels.length == 0 || labels.length % 5 === 0) ? Math.round(now * 1000) : null
+        labels.push = label
+        normalizedCooard.push(turfAlong(cooard, now))
+    }
+    const normalizedLevel = normalizedCooard.map((p) => { return p.geometry.coordinates[2]; })
+
+
+    // create graph
+    return new Chart(element, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: normalizedLevel,
+                backgroundColor: backgroundColor,
+                pointRadius: 0,
+            }]
+        },
+        options: {
+            title: {
+                display: false,
+            },
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    stacked: false,
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        drawBorder: false
+                    }
+                }]
+            }
+        }
+    })
+
+
+}
